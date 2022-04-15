4fe84fd0bd15894ece7a0d7911dea685aba85621
VasKho <vasya.khoroshavin@gmail.com>
2022-04-15 13:14:13 +0300

    [tools] Remove unused includes

--- sc-server/src/utils/parser.hpp
+++ sc-server/src/utils/parser.hpp
@@ -2,18 +2,15 @@
 #include <fstream>
 
 #include <boost/filesystem.hpp>
-#include <boost/dll/runtime_symbol_info.hpp>
 #include <boost/config.hpp>
 #include <boost/program_options/detail/config_file.hpp>
 #include <boost/program_options/parsers.hpp>
 
 namespace pod = boost::program_options::detail;
 namespace fs = boost::filesystem;
-namespace dll = boost::dll;
 
 std::map<std::string, std::string> parse_config(std::string conf) 
 {
-    // fs::path ostis_root = dll::program_location().remove_filename().parent_path();
     fs::path conf_directory(conf);
     conf_directory = conf_directory.parent_path();
 
