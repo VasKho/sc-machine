9678edb7cbbb1551a1002fc22011fe83bcc9cce7
VasKho <vasya.khoroshavin@gmail.com>
2022-04-15 13:13:15 +0300

    [tools][cmake] Fix config parsing to sctp-server

---
+++ sc-network/sctp_server/utils/parser.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+#include <boost/filesystem.hpp>
+#include <boost/config.hpp>
+#include <boost/program_options/detail/config_file.hpp>
+
+namespace pod = boost::program_options::detail;
+namespace fs = boost::filesystem;
+
+std::map<std::string, std::string> parse_config(std::string conf) 
+{
+    fs::path conf_directory(conf);
+    conf_directory = conf_directory.parent_path();
+
+    std::ifstream config(conf);
+    if(!config)
+    {
+        std::cerr << "error" << std::endl;
+        return std::map<std::string, std::string>();
+    }
+
+    std::set<std::string> options;
+    options.insert("*");
+    std::map<std::string, std::string> result;
+    
+    try
+    {      
+        for (pod::config_file_iterator i(config, options), e; i != e; ++i)
+        {
+	    if (i->string_key == "Repo.Path") result.insert({"path", (conf_directory/i->value[0]).string()});
+	    if (i->string_key == "Extensions.Directory") result.insert({"ext", (conf_directory/i->value[0]).string()});
+            if (i->string_key == "Network.Port") result.insert({"port", i->value[0]});
+            if (i->string_key == "Repo.SavePeriod") result.insert({"save_period", i->value[0]});
+            if (i->string_key == "Stat.UpdatePeriod") result.insert({"update_period", i->value[0]});
+            if (i->string_key == "Stat.Path") result.insert({"stat_path", i->value[0]});
+        }
+    }
+    catch(std::exception& e)    
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return result;
+}
